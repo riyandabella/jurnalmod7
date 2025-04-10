e73dbd4fa8d3714e7496fea8ac25c48d6b9cddbe
riyandabella <riyandabella561@gmail.com>
2025-04-10 11:12:44 +0700

    menambhakan kelas glossaryItem

--- Program.cs
+++ Program.cs
@@ -73,11 +73,71 @@ class TeamMembers103022300001
     }
 }
 
+class Glossary
+{
+    public string title { get; set; }
+    public glossDiv GlossDiv { get; set; }
+}
+
+class glossDiv
+{
+    public string title { get; set; }
+    public glossList GlossList { get; set; }
+
+}
+
+class glossList
+{
+    public glossEntry GlossEntry { get; set; }
+}
+
+class glossEntry
+{
+    public string ID { get; set; }
+    public string SortAs { get; set; }
+    public string GlossTerm { get; set; }
+    public string Acronym { get; set; }
+    public string Abbrev { get; set; }
+    public glossDef GlossDef { get; set; }
+    public string GlossSee { get; set; }
+
+}
+
+class glossDef
+{
+    public string para { get; set; }
+    public string[] GlossSeeAlso { get; set; }
+
+}
+class GlossaryItem103022300001
+{
+    public Glossary glossary { get; set; }
+
+    public static void ReadJSON()
+    {
+        string JsonString = File.ReadAllText("C:\\Users\\riyan\\source\\repos\\jurnalmod7\\jurnal7_3_103022300001.json");
+
+        var glossaryItem = JsonSerializer.Deserialize<GlossaryItem103022300001>(JsonString);
+
+        Console.WriteLine("Glossary title : " + glossaryItem.glossary.title);
+        Console.WriteLine("Glossary GlossDiv : ");
+        Console.WriteLine("GlossDiv title : " + glossaryItem.glossary.GlossDiv.title);
+        Console.WriteLine("GlossDiv GlossList : ");
+        Console.WriteLine("GlossList GlossEntry : ");
+        Console.WriteLine("GlossEntry ID : " + glossaryItem.glossary.GlossDiv.GlossList.GlossEntry.ID);
+        Console.WriteLine("GlossEntry sortAs : " + glossaryItem.glossary.GlossDiv.GlossList.GlossEntry.SortAs);
+        Console.WriteLine("GlossEntry glossTerm : " + glossaryItem.glossary.GlossDiv.GlossList.GlossEntry.GlossTerm);
+        Console.WriteLine("GlossEntry acronym : " + glossaryItem.glossary.GlossDiv.GlossList.GlossEntry.Acronym);
+
+    }
+}
+
 class Program
 {
     public static void Main()
     {
         DataMahasiswa103022300001.ReadJSON();
         TeamMembers103022300001.ReadJSON();
+        GlossaryItem103022300001.ReadJSON();
     }
 }
